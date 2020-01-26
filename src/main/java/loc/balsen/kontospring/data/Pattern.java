1974577b8bfe03459ca061d6e02f9d36a52a9716
balsen <balsen@balsen.loc>
2020-01-26 15:22:44 +0100

    fixed bug in reading pattern form older databases

--- src/main/java/loc/balsen/kontospring/data/Pattern.java
+++ src/main/java/loc/balsen/kontospring/data/Pattern.java
@@ -3,6 +3,7 @@ package loc.balsen.kontospring.data;
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Data;
@@ -16,39 +17,49 @@ public class Pattern {
 	private String senderID;
 	private String details;
 	private String mandate;
-	
-	static private final ObjectMapper mapper =  new ObjectMapper();
-	
+
+	static private final ObjectMapper mapper = new ObjectMapper();
+
 	public Pattern() {
 		sender = "";
-		receiver= "";
+		receiver = "";
 		referenceID = "";
-		senderID = ""; 
+		senderID = "";
 		details = "";
 		mandate = "";
 	};
 
+	private String getField(JsonNode p, String fieldname) {
+		JsonNode res = p.get(fieldname);
+		return (res == null) ? "" : res.asText();
+	}
+
 	public Pattern(String jsonString) {
 		try {
-			Pattern p = mapper.readValue(jsonString, Pattern.class);
-			this.sender = p.sender;
-			this.receiver = p.receiver;
-			this.referenceID = p.referenceID;
-			this.senderID = p.senderID;
-			this.details = p.details;
-			this.mandate = p.mandate;
-			
+			JsonNode p = mapper.readTree(jsonString);
+			this.sender = getField(p, "sender");
+			this.receiver = getField(p, "receiver");
+			this.referenceID = getField(p, "referenceID");
+			this.senderID = getField(p, "senderID");
+			this.details = getField(p, "details");
+			this.mandate = getField(p, "mandate");
+
+			// some old data from previous versions
+			if (this.mandate.isEmpty()) {
+				this.mandate = getField(p, "mandat");
+			}
+
 		} catch (IOException e) {
 			// TODO generate some log
 			sender = "";
-			receiver= "";
+			receiver = "";
 			referenceID = "";
-			senderID = ""; 
+			senderID = "";
 			details = "";
 			mandate = "";
 		}
 	}
-	
+
 	public Pattern(AccountRecord accountRecord) {
 		this.sender = accountRecord.getSender();
 		this.receiver = accountRecord.getReceiver();
@@ -69,15 +80,12 @@ public class Pattern {
 	}
 
 	public boolean matches(AccountRecord record) {
-		return matches(sender,record.getSender()) 
-				&& matches(receiver, record.getReceiver())
-				&& matches(referenceID , record.getReference())
-				&& matches(senderID , record.getSubmitter())
-				&& matches(details , record.getDetailsNOLF())
-				&& matches(mandate , record.getMandate());
+		return matches(sender, record.getSender()) && matches(receiver, record.getReceiver())
+				&& matches(referenceID, record.getReference()) && matches(senderID, record.getSubmitter())
+				&& matches(details, record.getDetailsNOLF()) && matches(mandate, record.getMandate());
 	}
 
 	private boolean matches(String pattern, String text) {
-		return ((text  == null || text.isEmpty()) && (pattern == null || pattern.isEmpty()) ) || text.contains(pattern);
+		return ((text == null || text.isEmpty()) && (pattern == null || pattern.isEmpty())) || text.contains(pattern);
 	}
 }
