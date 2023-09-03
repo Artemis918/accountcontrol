6bd724d864ecc4792f91a2a73ff0c8fcd4d86614
Dieter Balsen <dieter.balsen@gmail.com>
2023-09-03 18:22:11 +0200

    fixed timeunitselector

--- src/main/js/utils/timeunitselector.tsx
+++ src/main/js/utils/timeunitselector.tsx
@@ -11,15 +11,17 @@ export interface TimeUnitProps {
 
 export function TimeUnitSelector( props: TimeUnitProps) :JSX.Element {
 	const intl: IntlShape = useIntl();
+	function getName(key:string):string {return intl.formatMessage({id: key})};
+	
 	return (
             <select className={props.className}
                     value={ props.curvalue } 
                     onChange={( e: React.ChangeEvent<HTMLSelectElement> ) => 
                                     props.onChange(parseInt( e.target.value ))}>
-                <option key={0} value={0}> {intl.messages["day"]} </option> 
-                <option key={1} value={1}> {intl.messages["week"]} </option> 
-                <option key={2} value={2}> {intl.messages["month"]} </option> 
-                <option key={3} value={3}> {intl.messages["year"]} </option> 
+                <option key={0} value={0}> {intl.formatMessage({id: "day"})} </option> 
+                <option key={1} value={1}> {intl.formatMessage({id: "week"})} </option> 
+                <option key={2} value={2}> {intl.formatMessage({id: "month"})} </option> 
+                <option key={3} value={3}> {intl.formatMessage({id: "year"})} </option> 
             </select>
 	)
 }
