2103fc7c55114f6bff4d5f292b6874dc708d9b58
Dieter Balsen <dieter.balsen@gmail.com>
2024-02-11 21:41:25 +0100

    removed sql schema representation

--- scheme/createCategory.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.category (
-	id int4 NOT NULL,
-	description varchar(80) NULL,
-	shortdescription varchar(512) NULL,
-	CONSTRAINT category_pkey PRIMARY KEY (id)
-);
-
-create unique index category_id on public.category(id);
-create sequence public.seq_category;
--- scheme/createDB.sh
+++
@@ -1,7 +0,0 @@
-cat droptables_sql \
-    createCategory.sql \
-    createSubCategory.sql \
-    createTemplate.sql \
-    createPlan.sql \
-    createAccountRecord.sql \
-    createAssignment.sql | psql -U account -h localhost -p 5432 -d accounttest
