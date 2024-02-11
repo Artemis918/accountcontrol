c2ee7563bc859bbc92e91ae9b6f1f608cfd7a1ca
Dieter Balsen <dieter.balsen@gmail.com>
2024-02-11 06:23:20 +0100

    refactored ImportBaseTest

--- src/test/java/loc/balsen/accountcontrol/upload/ImportbaseTest.java
+++ src/test/java/loc/balsen/accountcontrol/upload/ImportbaseTest.java
@@ -1,34 +1,43 @@
 package loc.balsen.accountcontrol.upload;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.postgresql.util.PSQLException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
-import loc.balsen.accountcontrol.Application;
 import loc.balsen.accountcontrol.data.AccountRecord;
 import loc.balsen.accountcontrol.repositories.AccountRecordRepository;
 
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = Application.class)
-@WebAppConfiguration
 public class ImportbaseTest {
 
-  @Autowired
-  private AccountRecordRepository accountRecordRepository;
+  @Mock
+  public AccountRecordRepository accountRecordRepository;
 
-  @Autowired
-  private ImportTest importer;
+  @InjectMocks
+  private ImportTest importer = new ImportTest();
+
+  private AutoCloseable closeable;
+
+  @BeforeEach
+  public void setup() {
+    closeable = MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  public void teardown() throws Exception {
+    closeable.close();
+  }
 
   @Test
   public void testSave() throws PSQLException {
@@ -36,27 +45,24 @@ public class ImportbaseTest {
     List<AccountRecord> result = null;
 
     LocalDate now = LocalDate.now();
-    LocalDate later = now.plusDays(5);
 
     AccountRecord record1 = createRecord("ich", "du", now, 100);
-    AccountRecord record2 = createRecord("ich", "du", now, 100);
+    AccountRecord record2 = createRecord("ich", "ihr", now, 101);
 
-    assertTrue(importer.save(record1));
-    result = accountRecordRepository.findAll();
-    assertEquals(1, result.size());
-    assertNotNull(result.get(0).getId());
-    assertNotNull(record1.getId());
 
-    assertFalse(importer.save(null));
-    assertFalse(importer.save(record2));
-    assertEquals(1, result.size());
+    when(accountRecordRepository.findByValueAndCreatedAndSenderAndReceiver(eq(100), same(now),
+        eq("ich"), eq("du"))).thenReturn(new ArrayList<AccountRecord>());
 
-    assertTrue(importer.save(createRecord("ich", "du", now, 101)));
-    assertTrue(importer.save(createRecord("ich", "du", later, 100)));
-    assertTrue(importer.save(createRecord("er", "du", now, 100)));
-    assertTrue(importer.save(createRecord("ich", "er", now, 100)));
-    result = accountRecordRepository.findAll();
-    assertEquals(5, result.size());
+    List<AccountRecord> res = new ArrayList<>();
+    res.add(record2);
+    when(accountRecordRepository.findByValueAndCreatedAndSenderAndReceiver(eq(101), same(now),
+        eq("ich"), eq("ihr"))).thenReturn(res);
+
+    importer.save(record1);
+    verify(accountRecordRepository, times(1)).save(any(AccountRecord.class));
+
+    importer.save(record2);
+    verify(accountRecordRepository, times(1)).save(any(AccountRecord.class));
 
   }
 
