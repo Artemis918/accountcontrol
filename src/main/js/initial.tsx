b052f16c99e0d46f724c1850b3f2f2120bafb35e
Dieter Balsen <dieter.balsen@gmail.com>
2025-09-28 20:50:45 +0200

    some upgrades on initial page

--- src/main/js/initial.tsx
+++ src/main/js/initial.tsx
@@ -3,7 +3,7 @@ import { FormattedMessage } from 'react-intl';
 
 import css from './css/initial.css';
 
-type SetPage = ( page: number ) => void;
+type SetPage = (page: number) => void;
 
 interface InitialProps {
     setPage: SetPage;
@@ -11,49 +11,38 @@ interface InitialProps {
 
 export class InitialPage extends React.Component<InitialProps, {}> {
 
-    constructor( props: InitialProps ) {
-        super( props );
+    constructor(props: InitialProps) {
+        super(props);
     }
-    
-    render(): React.JSX.Element {
+
+    renderButton(idx: number, id: string): React.JSX.Element {
         return (
-            <table className={css.maintable}>
-                <tbody>
-                    <tr>
-                        <td> 
-                            <button id={'planbutton'} className={css.acbutton} onClick={() => this.props.setPage( 0 )}> 
-                              <FormattedMessage id="page.plan" defaultMessage="planing"/>
-                            </button>
-                        </td>
-                        <td> 
-                            <button className={css.acbutton} onClick={() => this.props.setPage( 1 )}> 
-                              <FormattedMessage id="page.accountRecords" defaultMessage="accountRecords"/>
-                            </button>
-                        </td>
-                        <td> 
-                            <button className={css.acbutton} onClick={() => this.props.setPage( 2 )}> 
-                                <FormattedMessage id="page.assign" defaultMessage="assign"/>
-                            </button>  </td>
-                    </tr>
-                    <tr>
-                        <td> 
-                            <button className={css.acbutton} onClick={() => this.props.setPage( 3 )}> 
-                                <FormattedMessage id="page.check" defaultMessage="check"/>
-                            </button>
-                        </td>
-                        <td> 
-                            <button className={css.acbutton} onClick={() => this.props.setPage( 4 )}> 
-                                 <FormattedMessage id="page.overview" defaultMessage="overview"/>
-                            </button>
-                        </td>
-                        <td> 
-                            <button className={css.acbutton} onClick={() => this.props.setPage( 5 )}>
-                                <FormattedMessage id="page.configuration" defaultMessage="configuration"/>
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <button id={id} className={css.acbutton} onClick={() => this.props.setPage(idx)}>
+                <FormattedMessage id={"page." + id} defaultMessage={id} />
+            </button>
         );
     }
-}
+
+
+    render(): React.JSX.Element {
+        return (
+            <div>
+                <div className={css.title}> <FormattedMessage id={"page.welcome"} defaultMessage={'Welcome to'} />&nbsp; AccountControl </div>
+                <table className={css.maintable}>
+                    <tbody>
+                        <tr>
+                            <td className={css.buttonfield}> {this.renderButton(0, 'plan')} </td>
+                            <td className={css.buttonfield}> {this.renderButton(1, 'accountRecords')} </td>
+                            <td className={css.buttonfield}> {this.renderButton(2, 'assign')} </td>
+                        </tr>
+                        <tr>
+                            <td className={css.buttonfield}> {this.renderButton(3, 'check')} </td>
+                            <td className={css.buttonfield}> {this.renderButton(4, 'overview')} </td>
+                            <td className={css.buttonfield}> {this.renderButton(5, 'configuration')} </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div >
+        );
+    }
+}    
